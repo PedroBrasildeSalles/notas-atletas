fd3dd24d6da79a56b5498437a7eeea5035099080
PedroBrasildeSalles <162842046+PedroBrasildeSalles@users.noreply.github.com>
2024-03-12 20:05:22 -0300

    Update notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -16,3 +16,21 @@ let atletas = [
    notas: [10, 10, 10, 9, 9.5]
  }
 ];
+
+atletas.forEach(function(atletas) {
+  let notasordenadas = atletas.notas.sort((a,b) => {
+    if(a > b) return 1;
+    if(a < b) return -1;
+    return 0;
+  })
+  let notasordenadasvalidas = notasordenadas.slice(1,-1)
+  let somaDasNotas = 0
+  for (let i = 0; i < notasordenadasvalidas.length; i++) {
+  somaDasNotas += notasordenadasvalidas[i];
+}
+  let medias = somaDasNotas / notasordenadasvalidas.length;
+return console.log(`Atleta:  ${atletas.nome}
+Notas Obtidas: ${atletas.notas}
+Media válida: ${medias}`)
+})
+
